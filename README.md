6c3e8e26478839031f7a88a50e29d2d40b6c4011
fcoantonioo <paiva.fcoantonio@gmail.com>
2024-07-14 21:22:54 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# IEL - Digital Store
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import GlobalStyle from "./components/GlobalStyles"
+
+function App() {
+
+  return (
+    <>
+      <GlobalStyle />
+      <h1>Hello React!</h1>
+    </>
+  )
+}
+
+export default App;
