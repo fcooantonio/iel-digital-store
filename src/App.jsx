8727adbf821172b59ae2588836cae148712656f2
fcoantonioo <paiva.fcoantonio@gmail.com>
2024-07-18 23:25:53 -0300

    update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,14 @@
-import styled from "styled-components";
 import GlobalStyle from "./components/GlobalStyles"
 import Paths from "./routes/Paths";
-import { device } from "./device";
 
 function App() {
 
   return (
-    <AppContainer>
+    <>
       <GlobalStyle />
       <Paths />
-    </AppContainer>
+    </>
   )
 }
-
-const AppContainer = styled.div`
-  @media ${device.laptop} {
-    & header {
-      padding: 17px 50px 14px;
-      & div input {
-        width: 280px;
-      }
-    }
-  }
-`;
 
 export default App;
