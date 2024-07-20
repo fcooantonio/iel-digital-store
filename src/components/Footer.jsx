2a412c285fee04e2b0bc1ffad56bca5274299aa1
fcoantonioo <paiva.fcoantonio@gmail.com>
2024-07-19 21:48:22 -0300

    footer base layout done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
             <section>
                 <div className="digital-store-infos">
                     <img src={logoFooter} alt="Digital Store Logo" />
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem inventore corrupti est maiores magnam eum culpa numquam magni sit autem libero molestias eos aut, dicta illum qui quia a quibusdam?</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
                     <ul>
                         <li>
                             <Link to="https://www.facebook.com/"><img src={logoFacebook} alt="logo facebook" /></Link>
@@ -26,9 +26,28 @@ const Footer = () => {
                         </li>
                     </ul>
                 </div>
-                <div></div>
-                <div></div>
-                <div></div>
+                <div>
+                    <h5>Informação</h5>
+                    <Link>Sobre Drip Store</Link>
+                    <Link>Segurança</Link>
+                    <Link>Wishlist</Link>
+                    <Link>Blog</Link>
+                    <Link>Trabalhe conosco</Link>
+                    <Link>Meus Pedidos</Link>  
+                </div>
+                <div>
+                    <h5>Categorias</h5>
+                    <Link>Camisetas</Link>
+                    <Link>Calças</Link>
+                    <Link>Bonés</Link>
+                    <Link>Headphones</Link>
+                    <Link>Tênis</Link>
+                </div>
+                <div>
+                    <h5>Contato</h5>
+                    <p>Av. Santos Dumont, 1510 - 1 andar - Aldeota, Fortaleza - CE, 60150-161</p>
+                    <p>(85) 3051-3411</p>
+                </div>
             </section>
             <h6>@ {dataAtual.getFullYear()} Digital College</h6>
         </FooterContainer>
@@ -50,31 +69,60 @@ const FooterContainer = styled.footer`
     & section {
         width: 100%;
         display: flex;
-        align-items: center;
+        align-items: flex-start;
         justify-content: space-between;
         border-bottom: 1px solid #ffffff30;
     }
-    & div {
-        width: 100px;
-        height: 10px;
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-    }
     & .digital-store-infos  {
         width: 304px;
-        height: 304px;
+        height: fit-content;
         display: flex;
         align-items: flex-start;
         justify-content: flex-start;
-        gap: 35px;
         flex-direction: column;
-        & ul {
-            width: 30%;
-            display: flex;
-            justify-content: space-between;
-            flex-direction: row;
+        & > img {
+            width: 253px;
+            height: 44px;
+            margin-bottom: 35px;
         }
+        & p {
+            width: 307px;
+            margin-bottom: 40px;
+            font-size: 1rem;
+            font-weight: 400;
+            line-height: 26px;
+            text-align: left;
+        }
+        & ul {
+            width: 124px;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+        }
+    }
+    & div:nth-child(2), div:nth-child(3), div:nth-child(4) {
+        width: 135px;
+        height: fit-content;
+        display: flex;
+        align-items: flex-start;
+        justify-content: flex-start;
+        flex-direction: column;
+        font-size: 1rem;
+        font-weight: 400;
+        line-height: 38px;
+        text-align: left;
+        & h5 {
+            font-size: 1.125rem;
+            font-weight: 600;
+            line-height: 22px;
+            text-align: left;
+        }
+    }
+    & div:nth-child(3) {
+        width: 96px;
+    } 
+    & div:nth-child(4) {
+        width: 233px;
     }
     & h6 {
         width: 100%;
